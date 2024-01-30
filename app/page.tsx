e1aeffafb49110c10b14fcabfdfd4aa88b00f2b1
Aris <armando.arismendi.23@gmail.com>
2024-01-30 10:03:39 -0300

    Primer componente con typescript

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { RandomFox } from "@/component/RandomFox";
 
 export default function Home() {
   return (
     <main>
       <h1>Hola mundo</h1>
+      <RandomFox/>
     </main>
   );
 }
